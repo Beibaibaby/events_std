8953d536600b03205cc91d5d4c13e9c60a128eed
Draco Xu <xuyunlongbeibai@gmail.com>
2024-09-24 17:50:27 -0500

    commit

--- no_need/src/test_2.jl
+++
@@ -1,8 +0,0 @@
-using JLD2
-
-# Path to one of your JLD2 files
-file_path = "/gpfs/data/doiron-lab/draco/results_corr/exp_20231114_233353/1/cross_corr_E_E.jld2"
-
-jldopen(file_path, "r") do file
-    println(keys(file))
-end
